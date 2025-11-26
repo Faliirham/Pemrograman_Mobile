dce180942aa53064d4dd21a5fff3b3de2c03bc27
FaliIrham <falimagemaulana64@gmail.com>
2025-11-26 12:44:57 +0700

    CODELAB 13

---
+++ src week-12/stream_fali/android/app/src/main/kotlin/com/example/stream_fali/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.stream_fali
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ src week-13/store_data_fali/android/app/src/main/kotlin/com/example/store_data_fali/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.store_data_fali
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
